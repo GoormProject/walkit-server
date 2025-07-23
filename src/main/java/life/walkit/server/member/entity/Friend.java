87eb699b30a30861b69ee070342ebec8d287133f
Woochang Lee <changi112242@gmail.com>
2025-07-23 11:21:45 +0900

    feat: 친구 Entity 추가

---
+++ src/main/java/life/walkit/server/member/entity/Friend.java
@@ -0,0 +1,48 @@
+package life.walkit.server.member.entity;
+
+import jakarta.persistence.*;
+import life.walkit.server.global.BaseEntity;
+import life.walkit.server.member.entity.enums.FriendStatus;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "friend")
+public class Friend extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "friend_id")
+    private Long friendId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "partner_id", nullable = false)
+    private Member partner;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private FriendStatus status;
+
+    @Builder
+    public Friend(Member member, Member partner) {
+        this.member = member;
+        this.partner = partner;
+        this.status = FriendStatus.PENDING;
+    }
+
+    public void approve() {
+        this.status = FriendStatus.APPROVED;
+    }
+
+    public void reject() {
+        this.status = FriendStatus.REJECTED;
+    }
+}
