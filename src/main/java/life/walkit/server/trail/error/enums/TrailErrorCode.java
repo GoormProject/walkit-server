ad3448c9947d2815d4025429514a235baa13ec77
한상희 <tkdgml822@gmail.com>
2025-07-23 23:08:57 +0900

    feat: 산책로 에러 코드 구현

--- src/main/java/life/walkit/server/trail/error/enums/TrailErrorCode.java
+++ src/main/java/life/walkit/server/trail/error/enums/TrailErrorCode.java
@@ -1,8 +1,25 @@
 package life.walkit.server.trail.error.enums;
 
+import life.walkit.server.global.error.core.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 
-// TODO: ErrorCode merge되면 Refactor
 @RequiredArgsConstructor
-public enum TrailErrorCode {
+public enum TrailErrorCode implements ErrorCode {
+    TRAIL_LIST_SELECT_FAILED(HttpStatus.NOT_FOUND, "산책로 목록 조회 실패"),
+    TRAIL_SELECT_FAILED(HttpStatus.NOT_FOUND,"산책로 조회 실패"),
+    TRAIL_CREATE_FAILED(HttpStatus.BAD_REQUEST, "산책로 등록 실패");
+
+    private final HttpStatus status;
+    private final String message;
+
+    @Override
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
 }
