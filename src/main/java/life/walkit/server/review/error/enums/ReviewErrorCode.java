5d28e82afb452fdd4411724c7b88105554025018
한상희 <tkdgml822@gmail.com>
2025-07-23 22:37:11 +0900

    feat: 리뷰 에러 코드 구현

--- src/main/java/life/walkit/server/review/error/enums/ReviewErrorCode.java
+++ src/main/java/life/walkit/server/review/error/enums/ReviewErrorCode.java
@@ -1,6 +1,26 @@
 package life.walkit.server.review.error.enums;
 
-// TODO: ErrorCode merge되면 Refactor
-public enum ReviewErrorCode {
+import life.walkit.server.global.error.core.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 
+@RequiredArgsConstructor
+public enum ReviewErrorCode implements ErrorCode {
+    REVIEW_SELECT_FAILED(HttpStatus.NOT_FOUND, "리뷰 조회에 실패했습니다."),
+    REVIEW_CREATE_FAILED(HttpStatus.BAD_REQUEST, "리부 생성에 실패했습니다."),
+    REVIEW_UPDATE_FAILED(HttpStatus.BAD_REQUEST, "리뷰 수정에 실패했습니다."),
+    REVIEW_DELETE_FAILED(HttpStatus.BAD_REQUEST, "리뷰 삭제에 실패했습니다.");
+
+    private final HttpStatus status;
+    private final String message;
+
+    @Override
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
 }
