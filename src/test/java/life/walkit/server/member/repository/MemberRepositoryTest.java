77a94243a294eb6fa1922c13108f7730610d22c3
Woochang Lee <changi112242@gmail.com>
2025-07-23 14:28:27 +0900

    test: 멤버 Repository 테스트 작성

---
+++ src/test/java/life/walkit/server/member/repository/MemberRepositoryTest.java
@@ -0,0 +1,79 @@
+package life.walkit.server.member.repository;
+
+import life.walkit.server.member.entity.Member;
+import life.walkit.server.member.entity.enums.MemberRole;
+import life.walkit.server.member.entity.enums.MemberStatus;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class MemberRepositoryTest {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @BeforeEach
+    void setUp() {
+        memberRepository.save(createMember("test@email.com", "테스트회원"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        memberRepository.deleteAllInBatch();
+    }
+
+    @Test
+    @DisplayName("멤버 저장 성공")
+    void save_success() {
+        // given
+        Member member = createMember("email@email.com", "닉네임");
+
+        // when
+        Member savedMember = memberRepository.save(member);
+
+        // then
+        Member foundMember = memberRepository.findById(savedMember.getMemberId()).get();
+        assertThat(foundMember)
+                .extracting("email", "nickname")
+                .containsExactly("email@email.com", "닉네임");
+    }
+
+    @Test
+    @DisplayName("email로 멤버 조회 성공")
+    void findByEmail_success() {
+        // when & then
+        Member foundMember = memberRepository.findByEmail("test@email.com").get();
+        assertThat(foundMember)
+                .extracting("email", "nickname")
+                .containsExactly("test@email.com", "테스트회원");
+    }
+
+    @Test
+    @DisplayName("nickname으로 멤버 조회 성공")
+    void findByNickname_success() {
+        // when & then
+        Member foundMember = memberRepository.findByNickname("테스트회원").get();
+        assertThat(foundMember)
+                .extracting("email", "nickname")
+                .containsExactly("test@email.com", "테스트회원");
+    }
+
+    private Member createMember(String email, String nickname) {
+        return Member.builder()
+                .email(email)
+                .name("김실명")
+                .nickname(nickname)
+                .status(MemberStatus.OFFLINE)
+                .role(MemberRole.USER)
+                .build();
+    }
+}
