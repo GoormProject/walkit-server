52709af9ffdb17fcc3ca94955eab2415d798912b
Bogeun Kim <mmmjjjkim@naver.com>
2025-07-23 14:04:41 +0900

    feat: WeatherController 추가

---
+++ src/main/java/life/walkit/server/weather/controller/WeatherController.java
@@ -0,0 +1,13 @@
+package life.walkit.server.weather.controller;
+
+import life.walkit.server.weather.service.WeatherService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/weather")
+public class WeatherController {
+    private final WeatherService weatherService;
+}
