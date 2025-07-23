a7a8e432347db869fe2dc1df6519c6bf1f7cde9f
Woochang Lee <changi112242@gmail.com>
2025-07-23 14:28:12 +0900

    feat: 멤버 Repository 추가

---
+++ src/main/java/life/walkit/server/member/repository/MemberRepository.java
@@ -0,0 +1,16 @@
+package life.walkit.server.member.repository;
+
+import life.walkit.server.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmail(String email);
+
+    Optional<Member> findByNickname(String nickname);
+
+}
