bf699f05e2a2aa356df89debbe52bb209bd7c48d
Bogeun Kim <mmmjjjkim@naver.com>
2025-07-23 16:55:17 +0900

    refactor: RequestMapping URI 수정

--- src/main/java/life/walkit/server/weather/controller/WeatherController.java
+++ src/main/java/life/walkit/server/weather/controller/WeatherController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/weather")
+@RequestMapping("/api/weather")
 public class WeatherController {
     private final WeatherService weatherService;
 }
