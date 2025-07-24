1afb9c206b481fc51ac7d847865afbd137c137b0
한상희 <tkdgml822@gmail.com>
2025-07-24 09:23:07 +0900

    fix: 오타 수정

--- src/main/java/life/walkit/server/review/error/enums/ReviewErrorCode.java
+++ src/main/java/life/walkit/server/review/error/enums/ReviewErrorCode.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ReviewErrorCode implements ErrorCode {
     REVIEW_SELECT_FAILED(HttpStatus.NOT_FOUND, "리뷰 조회에 실패했습니다."),
-    REVIEW_CREATE_FAILED(HttpStatus.BAD_REQUEST, "리부 생성에 실패했습니다."),
+    REVIEW_CREATE_FAILED(HttpStatus.BAD_REQUEST, "리뷰 생성에 실패했습니다."),
     REVIEW_UPDATE_FAILED(HttpStatus.BAD_REQUEST, "리뷰 수정에 실패했습니다."),
     REVIEW_DELETE_FAILED(HttpStatus.BAD_REQUEST, "리뷰 삭제에 실패했습니다.");
 
