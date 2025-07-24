5eb4d7067dd80a2db903c880a610d16a138b538e
한상희 <tkdgml822@gmail.com>
2025-07-24 11:21:26 +0900

    fix: 산책로 test 프로필 설정

--- src/test/java/life/walkit/server/trail/repository/TrailRepositoryTest.java
+++ src/test/java/life/walkit/server/trail/repository/TrailRepositoryTest.java
@@ -9,15 +9,17 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
-import static life.walkit.server.trail.factory.TrailTestFactory.createMember;
-import static life.walkit.server.trail.factory.TrailTestFactory.createTrail;
+import static life.walkit.server.global.factory.GlobalTestFactory.createMember;
+import static life.walkit.server.global.factory.GlobalTestFactory.createTrail;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class TrailRepositoryTest {
 
     @Autowired
