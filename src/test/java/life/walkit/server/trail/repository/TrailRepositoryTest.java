7251f11765634133c3d39f3a2f98a67add221a23
한상희 <tkdgml822@gmail.com>
2025-07-26 18:38:37 +0900

    test: 산책로 Path 엔티티 반영

--- src/test/java/life/walkit/server/trail/repository/TrailRepositoryTest.java
+++ src/test/java/life/walkit/server/trail/repository/TrailRepositoryTest.java
@@ -2,6 +2,8 @@ package life.walkit.server.trail.repository;
 
 import life.walkit.server.member.entity.Member;
 import life.walkit.server.member.repository.MemberRepository;
+import life.walkit.server.path.entity.Path;
+import life.walkit.server.path.repository.PathRepository;
 import life.walkit.server.trail.entity.Trail;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -13,8 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-import static life.walkit.server.global.factory.GlobalTestFactory.createMember;
-import static life.walkit.server.global.factory.GlobalTestFactory.createTrail;
+import static life.walkit.server.global.factory.GlobalTestFactory.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
 
@@ -28,32 +29,49 @@ public class TrailRepositoryTest {
 
     @Autowired
     private MemberRepository memberRepository;
+    @Autowired
+    private PathRepository pathRepository;
 
     @BeforeEach
     void setUp() {
-        memberRepository.save(createMember("a@email.com", "회원A"));
-        memberRepository.save(createMember("b@email.com", "회원B"));
+        Double[][] lineString = new Double[][]{
+                {126.986, 37.541},
+                {126.987, 37.542},
+                {126.988, 37.543},
+                {126.989, 37.544}
+        };
         memberRepository.save(createMember("c@email.com", "회원C"));
-
-        Member memberA = memberRepository.findByEmail("a@email.com").get();
-        Member memberB = memberRepository.findByEmail("b@email.com").get();
         Member memberC = memberRepository.findByEmail("c@email.com").get();
+        Path savePath = pathRepository.save(createPath(lineString));
 
-        trailRepository.save(createTrail(memberA, "인사동", "인사동 근처 산책로 입니다.", 3.2));
-        trailRepository.save(createTrail(memberB, "강남구", "강남구 근처 산책로 입니다.", 2.2));
-        trailRepository.save(createTrail(memberC, "도원동", "도원동 근처 산책로 입니다.", 1.2));
+        trailRepository.save(createTrail(memberC, "도원동", "도원동 근처 산책로 입니다.", 1.2, savePath));
     }
 
     @Test
     @DisplayName("산책로 저장 성공")
     void save_success() {
         // given
+        Double[][] lineString = new Double[][]{
+                {126.986, 37.541},
+                {126.987, 37.542},
+                {126.988, 37.543},
+                {126.989, 37.544}
+        };
         Member member = createMember("d@email.com", "회원임");
         Member savedMember = memberRepository.save(member);
         Member foundMember = memberRepository.findByEmail(savedMember.getEmail()).get();
-        
+        Path path = pathRepository.save(createPath(lineString));
+
         // when
-        Trail savedTrail = trailRepository.save(createTrail(foundMember, "해운대구", "해운대구 근처 산책로 입니다.", 3.2));
+        Trail savedTrail = trailRepository.save(
+            createTrail(
+                foundMember,
+                "해운대구",
+                "해운대구 근처 산책로 입니다.",
+                3.2,
+                path
+            )
+        );
 
         // then
         List<Trail> foundTrail = trailRepository.findByMember(savedTrail.getMember());
@@ -77,11 +95,45 @@ public class TrailRepositoryTest {
     @DisplayName("산책로 목록 조회")
     void findTrailList_success() {
         // given
+        Double[][] lineStringA = new Double[][]{
+                {126.986, 37.541},
+                {126.987, 37.542},
+                {126.988, 37.543},
+                {126.989, 37.544}
+        };
+        Double[][] lineStringB = new Double[][]{
+                {126.986, 37.541},
+                {126.987, 37.542},
+                {126.988, 37.543},
+                {126.989, 37.544}
+        };
+
+        Path pathA = pathRepository.save(createPath(lineStringA));
+        Path pathB = pathRepository.save(createPath(lineStringB));
+        Path foundPathA = pathRepository.findById(pathA.getPathId()).get();
+        Path foundPathB = pathRepository.findById(pathB.getPathId()).get();
+
         Member member = createMember("d@email.com", "회원임");
         Member savedMember = memberRepository.save(member);
         Member foundMember = memberRepository.findByEmail(savedMember.getEmail()).get();
-        Trail savedTrailA = trailRepository.save(createTrail(foundMember, "해운대구", "해운대구 근처 산책로 입니다.", 3.2));
-        Trail savedTrailB = trailRepository.save(createTrail(foundMember, "동성로", "동성로 근처 산책로 입니다.", 2.2));
+        Trail savedTrailA = trailRepository.save(
+            createTrail(
+                foundMember,
+                "해운대구",
+                "해운대구 근처 산책로 입니다.",
+                3.2,
+                foundPathA
+            )
+        );
+        Trail savedTrailB = trailRepository.save(
+            createTrail(
+                foundMember,
+                "동성로",
+                "동성로 근처 산책로 입니다.",
+                2.2,
+                foundPathB
+            )
+        );
 
         // when
         List<Trail> trailList = trailRepository.findByMember(foundMember);
