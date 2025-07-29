7f19a95bec907ea9cba104930eedc353fd449719
Bogeun Kim <mmmjjjkim@naver.com>
2025-07-29 12:45:56 +0900

    feat: 친구 요청 수신 목록 조회 API 구현

--- src/main/java/life/walkit/server/friend/controller/FriendController.java
+++ src/main/java/life/walkit/server/friend/controller/FriendController.java
@@ -3,6 +3,7 @@ package life.walkit.server.friend.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import life.walkit.server.auth.dto.CustomMemberDetails;
 import life.walkit.server.friend.dto.FriendRequestResponseDTO;
+import life.walkit.server.friend.dto.ReceivedFriendResponse;
 import life.walkit.server.friend.dto.SentFriendResponse;
 import life.walkit.server.friend.enums.FriendRequestResponse;
 import life.walkit.server.friend.service.FriendService;
@@ -11,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -21,10 +23,7 @@ public class FriendController {
 
     private final FriendService friendService;
 
-    /**
-     * 친구 요청 보내기 (생성)
-     */
-    @Operation(summary = "친구 요청 보내기", description = "다른 사용자에게 친구 요청을 보냅니다.")
+    @Operation(summary = "친구 요청 생성", description = "다른 사용자에게 친구 요청을 보냅니다.")
     @PostMapping("/requests")
     public ResponseEntity<BaseResponse> sendFriendRequest(
             @AuthenticationPrincipal CustomMemberDetails member,
@@ -33,17 +32,21 @@ public class FriendController {
         return BaseResponse.toResponseEntity(FriendRequestResponse.REQUEST_SUCCESS, response);
     }
 
-    /**
-     * 내가 보낸 친구 요청 목록 조회
-     */
     @GetMapping("/requests/sent")
-    @Operation(summary = "보낸 친구 요청 목록 조회", description = "내가 보낸 친구 요청 목록을 조회합니다.")
+    @Operation(summary = "친구 요청 발신 목록 조회", description = "내가 보낸 친구 요청 목록을 조회합니다.")
     public ResponseEntity<BaseResponse> getSentFriendRequests(
             @AuthenticationPrincipal CustomMemberDetails member) {
         List<SentFriendResponse> responses = friendService.getSentFriendRequests(member.getMemberId());
         return BaseResponse.toResponseEntity(FriendRequestResponse.SENT_LIST_SUCCESS, responses);
     }
 
+    @GetMapping("/requests/received")
+    @Operation(summary = "친구 요청 수신 목록 조회", description = "내가 받은 친구 요청 목록을 조회합니다.")
+    public ResponseEntity<BaseResponse> getReceivedFriendRequests(
+            @AuthenticationPrincipal CustomMemberDetails member) {
+        List<ReceivedFriendResponse> responses = friendService.getReceivedFriendRequests(member.getMemberId());
+        return BaseResponse.toResponseEntity(FriendRequestResponse.RECEIVED_LIST_SUCCESS, responses);
+    }
 
 
 }
