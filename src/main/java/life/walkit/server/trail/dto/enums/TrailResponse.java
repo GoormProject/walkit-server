5ca2506801e70ee8eddf4ebbadb7e8d095460679
한상희 <tkdgml822@gmail.com>
2025-08-04 22:08:37 +0900

    fix: 생성 enums HTTP 상태 변경

--- src/main/java/life/walkit/server/trail/dto/enums/TrailResponse.java
+++ src/main/java/life/walkit/server/trail/dto/enums/TrailResponse.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum TrailResponse implements Response {
 
-    TRAIL_CREATE_SUCCESS(HttpStatus.OK, "산책로 생성 성공"),
+    TRAIL_CREATE_SUCCESS(HttpStatus.CREATED, "산책로 생성 성공"),
     TRAIL_DETAIL_SELECT_SUCCESS(HttpStatus.OK, "산책로 상세 조회 성공"),
     GET_TRAIL_LIST_SUCCESS(HttpStatus.OK, "산책로 목록 조회 성공");
 
