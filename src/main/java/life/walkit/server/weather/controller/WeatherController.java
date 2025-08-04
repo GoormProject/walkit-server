9b1d73e49c359b5e804a3daf3ac15e3f368cdc82
Bogeun Kim <mmmjjjkim@naver.com>
2025-08-04 09:44:53 +0900

    feat: 날씨 조회 api 구현

--- src/main/java/life/walkit/server/weather/controller/WeatherController.java
+++ src/main/java/life/walkit/server/weather/controller/WeatherController.java
@@ -1,13 +1,33 @@
 package life.walkit.server.weather.controller;
 
+import life.walkit.server.weather.dto.WeatherForecastResponseDto;
+import life.walkit.server.weather.dto.WeatherLocationRequestDto;
+//import life.walkit.server.weather.dto.WeatherResponseDto;
+import life.walkit.server.weather.entity.AdminArea;
+import life.walkit.server.weather.repository.AdminAreaRepository;
 import life.walkit.server.weather.service.WeatherService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/weather")
 public class WeatherController {
     private final WeatherService weatherService;
+    private final AdminAreaRepository adminAreaRepository;
+
+    @GetMapping("/location")
+    public ResponseEntity<WeatherForecastResponseDto> getWeatherByCurrentLocation(
+            @RequestParam double latitude,
+            @RequestParam double longitude
+    ) throws Exception {
+        AdminArea area = weatherService.findNearestAdminArea(latitude, longitude);
+        WeatherForecastResponseDto dto = weatherService.fetchForecasts(area);
+        return ResponseEntity.ok(dto);
+    }
+
+
+
+
 }
